4cdd0aaa75f70b498ac713dcabccf466e380335b
DoctorTone <drtonesoftware@gmail.com>
2020-11-23 00:59:31 +0000

    Add mouse controls.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import ReactDom from "react-dom";
 import React, { useRef, useState } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
+import { Canvas, useFrame, extend, useThree } from "react-three-fiber";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { PlaneBufferGeometry } from "three";
+
+extend({ OrbitControls });
+
+const CameraControls = () => {
+  // Get a reference to the Three.js Camera, and the canvas html element.
+  // We need these to setup the OrbitControls component.
+  // https://threejs.org/docs/#examples/en/controls/OrbitControls
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls = useRef();
+  useFrame((state) => controls.current.update());
+  return <orbitControls ref={controls} args={[camera, domElement]} />;
+};
 
 function Box(props) {
   // This reference will give us direct access to the mesh
@@ -42,6 +60,7 @@ function Plane(props) {
 function App() {
   return (
     <Canvas>
+      <CameraControls />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <Box position={[0, 0, 0]} />
